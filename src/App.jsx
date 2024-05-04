35ea129525764e658916db948deaa18f10dc4361
Badalgupta123 <badalgupta7898@gmail.com>
2024-05-04 16:37:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import Navbar from './components/Navbar'
+import Testimonial from './components/Testimonial'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Footer from './components/Footer'
+import CTA from './components/CTA'
+
+function App() {
+  
+  return (
+    
+    <div className="">
+      <Navbar />
+      <Hero />
+      <Features />
+      <Testimonial />
+      <CTA />
+      <Footer />
+    </div>
+    
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        primaryColor: "hsl(270, 100%, 50%)",
+        PrimaryColorLight: "hsl(270, 100%, 64%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+      },
+    },
+  },
+  plugins: [],
+}
+
